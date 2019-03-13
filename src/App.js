ffa5111251480d2ffda9c5ad8cf456797c9a85c4
unknown <calozano@ccxc.us>
2019-03-13 10:06:04 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import TableComponent from './Components/Table';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TableComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Table/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Table } from 'reactstrap';
+
+
+class TableComponent extends React.Component {
+    render() {
+        return (
+            <div>111111</div>
+        );
+    }
+}
+
+export default TableComponent;
